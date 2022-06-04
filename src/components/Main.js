b9fb1da06b6ba723238b34c212552a6d30496eb6
Soner Şimşek <sonersimsekhd@gmail.com>
2022-06-04 10:38:59 +0300

    update

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,62 +1,54 @@
 import React, { useRef, useState, useEffect } from 'react'
+import {  sendSignInLinkToEmail } from "firebase/auth";
+import { auth } from '../firebase/firebase';
 import "./styles/_main.scss"
 import "./styles/_input.scss"
-import { auth } from '../firebase/firebase';
 
 export default function Main() {
 
-    
-
-    const [email, setEmail] = useState(
-        window.localStorage.getItem("emailForSignIn") || ""
-    );
-    useEffect(() => {
-        // Get the saved email
-        const saved_email = window.localStorage.getItem("emailForSignIn");
-
-        // Verify the user went through an email link and the saved email is not null
-        if (auth.isSignInWithEmailLink(window.location.href) && !!saved_email) {
-            // Sign the user in
-            auth.signInWithEmailLink(saved_email, window.location.href);
-        }
-    }, []);
-
-
-    const updateEmail = (e) => {
-        setEmail(e.target.value);
+    const handleSubmit = (e) => {
+            e.preventDefault()
+    }
+    const [email, SetEmail] = useState(null)
+    const actionCodeSettings = {
+        // URL you want to redirect back to. The domain (www.example.com) for this
+        // URL must be in the authorized domains list in the Firebase Console.
+        url: 'https://werk-task.vercel.app/',
+        // This must be true.
+        handleCodeInApp: true,
+        iOS: {
+          bundleId: 'com.example.ios'
+        },
+        android: {
+          packageName: 'com.example.android',
+          installApp: true,
+          minimumVersion: '12'
+        },
+        dynamicLinkDomain: 'https://werk-task.vercel.app/'
       };
 
-    const trySignIn = async () => {
-        // If the user is re-entering their email address but already has a code
-        if (auth.isSignInWithEmailLink(window.location.href) && !!email) {
-            // Sign the user in
-            auth.signInWithEmailLink(email, window.location.href).catch((err) => {
-                console.log("An unknown error has occured");
-
-            });
-        } else {
-            auth
-                .sendSignInLinkToEmail(email, {
-                    url: "http://localhost:3000/",
-                    handleCodeInApp: true,
-                })
-                .then(() => {
-                    // Save the users email to verify it after they access their email
-                    window.localStorage.setItem("emailForSignIn", email);
-                })
-                .catch((err) => {
-                    console.log("An unknown error has occured");
-
-                });
-        }
-    };
-
-
+       async function login(){
+        await sendSignInLinkToEmail(auth, email, actionCodeSettings)
+        .then(() => {
+            // The link was successfully sent. Inform the user.
+            // Save the email locally so you don't need to ask the user for it again
+            // if they open the link on the same device.
+            window.localStorage.setItem('emailForSignIn', email);
+            // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorCode)
+            console.log(errorMessage)
+        });
+        alert("içerde")
+      }
     return (
-        <div className='main'>
-            <form action=''>
+        <div className='main' >
+            <form action='' onSubmit={handleSubmit}>
                 <h2 className='login-text'>Login</h2>
-                <input placeholder='E-mail' type="email" className='inputField' value={email}  onChange={updateEmail} onKeyPress={(e) => e.key === 'Enter' && trySignIn()}></input>
+                <input placeholder='E-mail' type="email" className='inputField' onChange={(e) => { SetEmail(e.target.value) }} onKeyPress={(e) => e.key === 'Enter' && login()}></input>
             </form>
         </div>
     )
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import { getFirestore } from 'firebase/firestore/lite';
+//, collection, getDocs
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -22,5 +23,5 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
-export { auth };
+export { auth ,};
 export default db;
