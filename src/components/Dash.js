b998822878b755bf04ed8373bb2b903be6fbfe86
Soner Şimşek <sonersimsekhd@gmail.com>
2022-06-06 21:10:48 +0300

    update

--- src/components/Dash.js
+++ src/components/Dash.js
@@ -4,17 +4,23 @@ import "./styles/_input.scss"
 import "./styles/_second.scss"
 import { useDispatch, useSelector } from 'react-redux';
 import { logout, selectUser } from '..//redux/features/userSlice';
-import { auth } from '../firebase/firebase';
-
+import { auth, db, collection} from '../firebase/firebase';
+import { doc, setDoc } from "firebase/firestore";
 export default function Dash() {
 
+    const usersCollectionRef = collection(db, "users");
+    
     const user = useSelector(selectUser);
     const dispatch = useDispatch();
     const logoutOfApp = () => {
       dispatch(logout());
       auth.signOut();
     };
-
+    async function postAdd () {
+        await setDoc(doc(db, "todos", "todo2"), {
+            task: "go inside",
+          });
+    }
     return (
         <>
           {user ? <div className='main todo-page'>
@@ -49,7 +55,7 @@ export default function Dash() {
                         <button className='button' type='button'>b</button>
                     </div>
                 </div>
-                <div><input placeholder='+New' type="email" className='inputField'></input></div>
+                <div><input placeholder='+New' type="email" className='inputField' onKeyPress={(e) => e.key === 'Enter' && postAdd()}></input></div>
                 <div><button  type="button" onClick={logoutOfApp} className='button'>Log Out</button></div>
             </div>
             : ''}
--- src/redux/features/userSlice.js
+++ src/redux/features/userSlice.js
@@ -1,22 +1,27 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-export const userSlice  = createSlice({
-    name: "user",
-    initialState: {
-        user:null,
+import { db } from "../../firebase/firebase";
+export const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    user: null,
+    tasks: [],
+  },
+  reducers: {
+    login: (state, action) => {
+      state.user = action.payload;
     },
-    reducers: {
-        login: (state, action) => {
-            state.user = action.payload;
-          },
-          logout: (state) => {
-            state.user = null;
-          },
-        list(action){},
-        todoAdd(action) {},
-        todoUpdated(action) {},
+    logout: (state) => {
+      state.user = null;
     },
-});
-export const { todoAdd, todoUpdated , login, logout, list } = userSlice.actions;
+    list(action) { },
+    todoAdd(action) {
+      // Add a new document in collection "cities"
+     
+    },
+    todoUpdated(action) {},
+    },
+  });
+export const { todoAdd, todoUpdated, login, logout, list } = userSlice.actions;
 export const selectUser = (state) => state.user.user;
 export default userSlice.reducer;
