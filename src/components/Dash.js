5264f0de252866d4e4950f39e51df9f364cec53c
Soner Şimşek <sonersimsekhd@gmail.com>
2022-06-09 11:10:29 +0300

    updated

--- src/components/Dash.js
+++ src/components/Dash.js
@@ -136,6 +136,7 @@ export default function Dash() {
                                         {toggle ? (
                                             task.map((item) => (
                                                 <li
+                                                    key={item.id}
                                                     onDoubleClick={() => {
                                                         setToggle(false);
                                                     }}
